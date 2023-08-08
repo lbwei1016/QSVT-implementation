35a279453c8fd8ce0bc3a279411d206e8e207324
lbwei <0800680274united@gmail.com>
2023-08-08 23:27:51 +0800

    patch

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ do
         record="./results_tmp/${filename}.txt"
         profile="./profiles_tmp/${filename}.prof"
 
-        time python3 -m cProfile -o "${profile}" qsvt-linear-solver.py -N ${N} -d ${d} > ${file}
+        time python3 -m cProfile -o "${profile}" qsvt-linear-solver.py -N ${N} -d ${d} > ${record}
 
         if [ ! -s "${record}" ];
         then
