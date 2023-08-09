a7c31e7f480a59064871a6ddf2efd8cf24d464d9
lbwei <0800680274united@gmail.com>
2023-08-09 09:39:11 +0800

    change run.sh

--- run.sh
+++ run.sh
@@ -3,26 +3,32 @@
 START=$1
 END=$2
 
-for N in $(seq $START $END)
+for d in {0..1}
 do
-    for d in {0..1}
+    flag="1"
+    for N in $(seq $START $END)
     do
         # d = 0: deg = 121; d = 1: deg = 601; d = 2: deg = 2501
         echo "Now running N = ${N}, set_deg = ${d}."
 
         filename="${N}q-nAA-${d}"
-        record="./results_tmp/${filename}.txt"
-        profile="./profiles_tmp/${filename}.prof"
+        record="./results_run/${filename}.txt"
+        profile="./profiles_run/${filename}.prof"
 
         time python3 -m cProfile -o "${profile}" qsvt-linear-solver.py -N ${N} -d ${d} > ${record}
 
         if [ ! -s "${record}" ];
         then
             echo "N = ${N} does not finish!" 
+            flag="0"
             break
         fi
 
-        echo "set_deg = ${d} done!"
+        echo "N = ${N}, set_deg = ${d} done!"
     done
-    echo "N = ${N} done!"
+
+    if [ ${flag} -eq "0" ]; then 
+        break
+    fi
+    # echo "N = ${N} done!"
 done
