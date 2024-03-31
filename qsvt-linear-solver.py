ed7afb8cad4310497afb7efe61787d49f158cac0
lbwei <0800680274united@gmail.com>
2024-03-31 16:33:00 +0800

    prettify qsvt-linear-solver.py

--- qsvt-linear-solver.py
+++ qsvt-linear-solver.py
@@ -1,43 +1,47 @@
-#from qiskit import QuantumCircuit
-#from qiskit.circuit import QuantumRegister, ClassicalRegister
-#from qiskit.extensions import UnitaryGate
 from qiskit.quantum_info import Statevector
 
 import numpy as np
 
-# from qsvt.algorithms import linear_solver
 from qsvt.algorithms import linear_solver
-from qsvt.helper import total_variation, gen_random_matrix
+from qsvt.helper import total_variation, random_matrix
 
-from qiskit import transpile
+from qiskit import transpile, QuantumCircuit, ClassicalRegister
 from qiskit_aer import AerSimulator
 from qiskit.visualization import plot_histogram
 
 import time
 import getopt, sys
 
-# import memory_profiler
-
+import memory_profiler
 # @memory_profiler.profile
-def exec():
-
-    TOTAL_TIME = 0
-    AA_On = False
-    set_degree = 0
-    simulation_method = 'statevector'
-    # matrix size exponent
-    N = 1
 
 
+OVERALL_TIME = 0
+AA_On = False
+set_degree = 0
+# simulation_method = 'statevector'
+MPI_NODES = 0
+SIMULATION = False
+# matrix size exponent
+N = 1
+
+def parse_cmd_parameters():
     # Remove 1st argument from the
     # list of command line arguments
     argumentList = sys.argv[1:]
 
     # Options
-    options = "hN:ad:s:"
+    options = "hN:ad:m:s"
 
     # Long options
-    long_options = ["help", "Num_of_qubits_for_matrix=", "AA", "set_degree"]
+    long_options = ["help", "num-of-qubits-for-matrix=", "AA", "set-degree", 'mpi', 'self-sampling']
+
+    global OVERALL_TIME
+    global AA_On
+    global set_degree
+    global MPI_NODES
+    global SIMULATION 
+    global N
 
     try:
         # Parsing argument
@@ -48,81 +52,77 @@ def exec():
 
             if currentArgument in ("-h", "--help"):
                 help_msg = """
-    -h: show help
+    -h: Show help
     -N <Number of qubits for matrix>: specify matrix size
     -a: Use AA
+    -m <Number of nodes>: enable MPI and specify the number of nodes to run on 
+    -s: Run simulation (with AerSimulator), rather than self-sample the statevetor
                 """
                 print(help_msg)
+                exit(0)
                 
-            elif currentArgument in ("-N", "--Num_of_qubits_for_matrix"):
+            elif currentArgument in ("-N", "--num-of-qubits-for-matrix"):
                 N = int(currentValue)
                 print(f'N = {currentValue}')
                 
             elif currentArgument in ("-a", "--AA"):
                 AA_On = True
                 print('AA is on')
-            elif currentArgument in ("-d", "--set_degree"):
+            elif currentArgument in ("-d", "--set-degree"):
                 set_degree = int(currentValue)
                 print(f"set_degree = {set_degree}")
-            # elif currentArgument in ("-s", "--simulation_method"):
-            #     if 
-            #     simulation_method = 
-            #     print(f'simulation_method = {simulation_method}')
+            elif currentArgument in ('-m', '--mpi'):
+                MPI_NODES = int(currentValue)
+                print(f'MPI is on with {MPI_NODES} nodes')
+            elif currentArgument in ('-s', 'self-sampling'):
+                SIMULATION = True
+                print('Experiment with AerSimulator')
                 
     except getopt.error as err:
         # output error, and return with an error code
-        print (str(err))
+        print(str(err))
 
-    #########################################################################
 
-    #N = 9
+def gen_random_unitary() -> np.ndarray:
     st = time.time()
-    A = gen_random_matrix(50-1e-9, 2**(N), N)
+    A = random_matrix(N)
     ed = time.time()
     print(f'generate matrix time spent: {ed - st} sec')
     print('==================================')
-
-
-    # A = np.array([
-    #     [1, -1/3],
-    #     [-1/3, 1]
-    # ])
 
     A_norm = np.linalg.norm(A)
     A /= A_norm
     print(f'A:\n{A}')
 
     print(f'calculating condition number...')
-    st = time.time()
     kappa = np.linalg.cond(A)
-    ed = time.time()
-    # print(f'time spent for calculating condition number: {ed - st} sec')
-    # print(f'kappa: {kappa}')
 
-    # W, S, Vd = np.linalg.svd(A)
-    # print(f'SVD of normalized A:\n\tW:\n{W}\n\tS:\n{S}\n\tVd:\n{Vd}')
+    return A
+
+
+def create_circuit(A: np.ndarray) -> QuantumCircuit:
+    global OVERALL_TIME
 
     st = time.time()
     if not AA_On:
         qc = linear_solver(A, set_degree=set_degree)
     else:
         qc = linear_solver(A, set_degree=set_degree, amplify='AA')
-    # qc = linear_solver(A, eps=0.01, set_kappa=True)
-    # qc = linear_solver(A, set_kappa=True)
-    # qc = linear_solver(A, set_kappa=True, amplify='sign')
-    # qc = linear_solver(A, amplify='sign')
-    # qc = linear_solver(A, real_only=False)
-    # qc = linear_solver(A, amplify='chebyshev')
-    # qc = linear_solver(A, amplify='sign')
-    # qc = linear_solver(A, amplify='AA')
     ed = time.time()
-    TOTAL_TIME += (ed - st)
-    print(f'prepare circuit spends: {ed - st} sec')
 
-    # print(f'circuit depth: {qc.depth()}')
+    OVERALL_TIME += (ed - st)
+
+    print(f'prepare circuit spends: {ed - st} sec')
+    print(f'circuit depth: {qc.depth()}')
     # qc.draw('mpl')
 
     print('==================================')
+
+    return qc
+
+
+def prepare_snapshot(A: np.ndarray, qc: QuantumCircuit):
+    global OVERALL_TIME
 
     st = time.time()
     state = Statevector(qc)
@@ -172,34 +172,30 @@ def exec():
     print('==================================')
     P = np.array([mstate[i] for i in range(2 ** N)])
     P = np.array([np.linalg.norm(x)**2 for x in P])
-    #print(f'P: {P}')
-    # res = [-0.63012604,  0.070014,    0.070014,    0.77015405]
     Q = np.array([x ** 2 for x in res])
-    #print(f'Q: {Q}')
 
-    print(f'kappa: {kappa}')
+    # print(f'kappa: {kappa}')
     print(f'total_variation (exact): {total_variation(P, Q)}')
-
     print('==================================')
 
-###########################################################################################
+    return Q
+
+
+def self_sampling(qc: QuantumCircuit, real_distribution: np.ndarray, shots: int=10000):
+    # global OVERALL_TIME
+    
     st = time.time()
     state = Statevector(qc)
     P = np.array([np.linalg.norm(x)**2 for x in state])
-    shots = 10000
-    # counts = {}
     
-    # print(len(state), len(list(range(2 ** N))))
+    n = qc.num_qubits
     a = np.random.choice(a=list(range(2 ** n)), p=P, size=shots)
     a = np.sort(a)
     unique_elements, counts = np.unique(a, return_counts=True)
     counts = dict(zip(unique_elements, counts))
-    # print(counts)
-    # plot_histogram(counts)
 
     ed = time.time()
     print(f'sampling time: {ed - st} sec')
-
     
 
     st = time.time()
@@ -207,96 +203,72 @@ def exec():
     valid_count = np.zeros(shape=(SIZE))
     tmp = 0
     for data in counts:
-        # tmp += counts[data]
-        # print(f'data: {data[:]}')
         if data < SIZE:
             tmp += counts[data]
-            # print(int(data[2:], base=2))
             valid_count[int(data)] = counts[data]
     valid_count /= shots
-    # valid_count /= np.linalg.norm(valid_count)
     valid_count /= np.sum(valid_count)
-    print(f'tmp: {tmp}')
+    # print(f'tmp: {tmp}')
     print(f'valid_count: {valid_count}; sum: {np.sum(valid_count)}')
 
-    # Q = np.array([x ** 2 for x in res])
-    tot_var = total_variation(valid_count, Q)
+    tot_var = total_variation(valid_count, real_distribution)
     ed = time.time()
     print(f'total var. (exp) time spent: {ed - st}')
-    #print(f'Q: {Q}')
 
-    print(f'kappa: {kappa}')
+    # print(f'kappa: {kappa}')
     print(f'total_variation (exp): {tot_var}')
 
     
+def simulation(qc: QuantumCircuit, shots: int=10000) -> list:
+    global OVERALL_TIME
 
-###############################################################################
-    # cr = ClassicalRegister(len(measure_qubits))
-    # qc.add_register(cr)
-    # qc.measure(measure_qubits, cr)
-    print('==================================')
-    # qc.measure_all()
-    # print(f'qc depth: {qc.depth()}')
+    qc.measure_all()
+    print(f'qc depth: {qc.depth()}')
 
-    # # It seems that even if 'GPU' is specified, GPU is not used at all.
-    # # Since QSVT involves large multi-qubit gates (block-encoding), "extended_stabilizer" is not efficient.
-    # # sim = AerSimulator(method='extended_stabilizer')
-    # sim = AerSimulator(method='statevector')
-    # # sim = AerSimulator(method='density_matrix')
-    # # sim = AerSimulator(method='statevector', device='GPU')
+    # It seems that even if 'GPU' is specified, GPU is not used at all.
+    # Since QSVT involves large multi-qubit gates (block-encoding), "extended_stabilizer" is not efficient.
+    # sim = AerSimulator(method='extended_stabilizer')
+    sim = AerSimulator(method='statevector')
+    # sim = AerSimulator(method='density_matrix')
+    # sim = AerSimulator(method='statevector', device='GPU')
 
-    # st = time.time()
+    st = time.time()
     # transpiled_circuit = transpile(qc, sim)
-    # # transpiled_circuit = transpile(qc, sim, optimization_level=3)
-    # ed = time.time()
-    # print(f'transpilation spends: {ed - st} sec')
-    # TOTAL_TIME += (ed - st)
-    # # transpiled_circuit = transpile(qc, sim, optimization_level=3)
-    # print(f'transpiled qc depth: {transpiled_circuit.depth()}')
+    transpiled_circuit = transpile(qc, sim, optimization_level=3)
+    ed = time.time()
+    print(f'transpilation spends: {ed - st} sec')
+    OVERALL_TIME += (ed - st)
+    print(f'transpiled qc depth: {transpiled_circuit.depth()}')
 
-    # # run job
-    # shots = 10000
-    # st = time.time()
-    # job = sim.run(transpiled_circuit, shots=shots, dynamic=True, blocking_enable=True, blocking_qubits=10)
+    # run job
+    st = time.time()
 
-    # # Get the results and display them
-    # exp_result = job.result()
-    # exp_counts = exp_result.get_counts()
-    # ed = time.time()
-    # TOTAL_TIME += (ed - st)
-    # print(f'run job spends: {ed - st} sec')
-    
-    
-    
-########################################################################
-    # plot_histogram(exp_counts)
-
-    # Calculate total variance
-    # experiment count
-    #print(f'exp_counts: {exp_counts}')
-
-    # st = time.time()
-    # valid_count = np.zeros(shape=(2 ** N))
-    # for data in exp_counts:
-    #     # print(f'data: {data[:]}')
-    #     if data[:2] == '00':
-    #         # print(int(data[2:], base=2))
-    #         valid_count[int(data[2:], base=2)] = exp_counts[data]
-    # valid_count /= shots
-    # valid_count /= np.linalg.norm(valid_count)
-    # #print(f'valid_count: {valid_count}')
-
-    # Q = np.array([x ** 2 for x in res])
-    # tot_var = total_variation(valid_count, Q)
-    # ed = time.time()
-    # print(f'total var. (exact) time spent: {ed - st}')
-    # #print(f'Q: {Q}')
-
-    # print(f'kappa: {kappa}')
-    # print(f'total_variation (exp): {tot_var}')
-    # print('==================================')
-
-    print(f'total execution time (exclude snapshot): {TOTAL_TIME} sec')
+    if MPI_NODES > 0:
+        # 'blocking_qubits=10' is set ignorantly here
+        job = sim.run(transpiled_circuit, shots=shots, dynamic=True, blocking_enable=True, blocking_qubits=10)
+    else:
+        job = sim.run(transpiled_circuit, shots=shots, dynamic=True)
 
 
-exec()
+    # Get the results and display them
+    exp_result = job.result()
+    exp_counts = exp_result.get_counts()
+    ed = time.time()
+    OVERALL_TIME += (ed - st)
+    print(f'run job spends: {ed - st} sec')
+    print("==========================================================")
+    print(f'total execution time (exclude snapshot): {OVERALL_TIME} sec')
+
+    return exp_counts
+
+
+if __name__ == '__main__':
+    parse_cmd_parameters()
+    A = gen_random_unitary()
+    qc = create_circuit(A)
+
+    if SIMULATION:
+        exp_counts = simulation(qc)
+    else:
+        Q = prepare_snapshot(A, qc)
+        self_sampling(qc, Q)
